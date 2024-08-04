613e56d9a128438a480e923f335d0b4c3829edf0
Alice Chan <alicechaninteng@gmail.com>
2024-08-04 14:03:12 -0400

    first commit

---
+++ build/depthai_examples/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/alicechan/dai_ws/install/depthai_descriptions/share/depthai_descriptions/package.sh"
+. "/home/alicechan/dai_ws/install/depthai_ros_msgs/share/depthai_ros_msgs/package.sh"
+. "/home/alicechan/dai_ws/install/depthai_bridge/share/depthai_bridge/package.sh"
